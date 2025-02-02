b95d33dc868236099afe282770b023d78b22f0c4
iQ <estrizqy@gmail.com>
2025-02-03 00:27:15 +0700

    style: changing header sidebar & remove social media login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -74,73 +74,65 @@
         </div>
     </div>
 </div> --}}
-<div class="hold-transition register-page">
-    <div class="register-box">
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+    <div class="hold-transition register-page">
+        <div class="register-box">
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <a href="../../index2.html" class="h1"><b>E</b>-Library</a>
+                </div>
+                <div class="card-body">
+                    <p class="login-box-msg">Register a new membership</p>
+
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" name="name" placeholder="Full name"
+                                autocomplete="name">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" name="email" placeholder="Email"
+                                autocomplete="email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" name="password" placeholder="Password"
+                                autocomplete="new-password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" name="password_confirmation"
+                                placeholder="Retype password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-end">
+                            <!-- /.col -->
+                            <div class="w-50">
+                                <button type="submit" class="btn btn-primary btn-block">Register</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+                    <a href={{ route('login') }} class="text-center">I already have a membership</a>
+                </div>
+                <!-- /.form-box -->
             </div>
-            <div class="card-body">
-                <p class="login-box-msg">Register a new membership</p>
-
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" name="name" placeholder="Full name" autocomplete="name">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-user"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="email" class="form-control" name="email" placeholder="Email" autocomplete="email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="new-password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-end">
-                    <!-- /.col -->
-                    <div class="w-50">
-                        <button type="submit" class="btn btn-primary btn-block">Register</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
-
-              <div class="social-auth-links text-center">
-                <a href="#" class="btn btn-block btn-primary">
-                  <i class="fab fa-facebook mr-2"></i>
-                  Sign up using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                  <i class="fab fa-google-plus mr-2"></i>
-                  Sign up using Google+
-                </a>
-              </div>
-
-              <a href={{ route('login') }} class="text-center">I already have a membership</a>
-            </div>
-            <!-- /.form-box -->
         </div>
     </div>
-</div>
 @endsection
--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -3,7 +3,7 @@
     <a href="index3.html" class="brand-link">
         <img src={{ asset('dashboard/img/AdminLTELogo.png') }} alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Perpustakaan</span>
+        <span class="brand-text font-weight-light">E-Library</span>
     </a>
 
     <!-- Sidebar -->
